24c32d12b8601c4aa12a70a1a9411c738515f602
Krtz_Dev <florian.krtz@gmail.com>
2022-11-06 15:16:43 +0100

    Game can be Won

---
+++ Assets/Scripts/Puzzle/Puzzlemanager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Puzzlemanager : MonoBehaviour
+{
+    public static Puzzlemanager Instance { get; private set; }
+
+    [SerializeField]
+    private int solvedPuzzlesToWin;
+
+    private int solvedPuzzles;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    public void PuzzleSolved()
+    {
+        solvedPuzzles++;
+        if (solvedPuzzles >= solvedPuzzlesToWin)
+        {
+            GameWon();
+        }
+    }
+
+    public void GameWon()
+    {
+        Debug.Log("Win");
+    }
+
+}
